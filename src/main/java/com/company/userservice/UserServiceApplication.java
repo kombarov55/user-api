e6380c1c81002976a94a5bef9daed12fb05c6c79
Nikolay <kombarov55@gmail.com>
2018-04-17 23:31:15 +0300

    added h2, codec, user class

--- src/main/java/com/company/userservice/UserServiceApplication.java
+++ src/main/java/com/company/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.company.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -11,3 +12,4 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }
+ 2
---
+++ src/main/java/com/company/userservice/model/User.kt
@@ -0,0 +1,7 @@
+package com.company.userservice.model
+
+import java.util.*
+
+data class User(val id: String, val firstName: String, val lastName: String, val birthday: Date, val email: String, val password: String) {
+
+}
